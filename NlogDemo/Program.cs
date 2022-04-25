98817cd8913a825de81c76736b92c6b2444e7448
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-04-25 13:15:29 +0530

    [Harshal]Add: Nlog File Are Added

---
+++ NlogDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using NlogDemo;
+using System;
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Program");
+            AddNumber number = new AddNumber();
+            number.Sum(1, 2);
+        }
+    }
+}
